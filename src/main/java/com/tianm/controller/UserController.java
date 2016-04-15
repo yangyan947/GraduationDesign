0562672518bdf6f0bd79576a5803e295dbb12181
sunyi <sunyi@shmingjiang.org.cn>
2016-04-15 15:09:14 +0800

    aaa

--- src/main/java/com/tianm/controller/UserController.java
+++ src/main/java/com/tianm/controller/UserController.java
@@ -1,49 +1,159 @@
 package com.tianm.controller;
 
 import com.tianm.domain.User;
-import com.tianm.repository.UserRepository;
 import com.tianm.service.UserService;
 import com.tianm.service.message.Message;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
  * Created by SunYi on 2016/3/24/0024.
  */
-@RestController
-@RequestMapping("/api/")
+@Controller
+@RequestMapping("/")
 public class UserController {
 
-
-    @RequestMapping(value = "user/{id}", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
-    public Message getByEmail(@PathVariable(value = "id") Long id) {
-        return userService.getById(id);
-    }
-
-    @RequestMapping(value = "login", method = RequestMethod.POST, produces = "application/json; charset=utf-8")
-    public Message login(@RequestParam(value = "email") String email,
-                         @RequestParam(value = "password") String password,
-                         HttpSession session) {
-        User user = new User(email, "", password);
-        Message message = userService.login(user);
-        session.setAttribute("user", message.getOthers());
-        return message;
-    }
-
-    @RequestMapping(value = "user", method = RequestMethod.POST, produces = "application/json; charset=utf-8")
-    public Message register(@RequestParam(value = "email") String email,
-                            @RequestParam(value = "name") String name,
-                            @RequestParam(value = "password") String password) {
-        User user = new User(email, name, password);
-        Message message = userService.register(user);
-        return message;
-    }
-
+    //自动注入userService，用来处理业务
     @Autowired
     private UserService userService;
-    @Autowired
-    private UserRepository userDao;
+//    @Autowired
+//    private ProjectService projectService;
+
+    //跳转链接，跳转到主页xx
+    @RequestMapping("/")
+    public String index(Model model,
+                        HttpServletResponse response) {
+        //对应到templates文件夹下面的index
+//        Message message = projectService.getAllProject();
+//        model.addAttribute("projects", message.getOthers());
+        return "index";
+    }
+
+    //真正主页，用户在访问 XXXXX/index就会跳转该方法，
+    // 这个XXXXX是你的域名，自己电脑上的话一般都是127.0.0.1:8080或者是localhost：8080
+    // 8080是端口号，端口号根据tomcat设置而改变，默认值是8080
+    @RequestMapping("/index")
+    public String home(Model model, @RequestParam(value = "result", defaultValue = "欢迎！") String result) {
+        //对应到templates文件夹下面的index
+        if (result != null) {
+            model.addAttribute("result", result);
+        }
+        return "index";
+    }
+
+    //进入注册页面，使用Get请求，REST风格的URL能更有雅的处理问题
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String registerGet() {
+        return "register";
+    }
+
+    //注册用户，使用POST，传输数据
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public RedirectView registerPost(Model model,
+                                     //这里和模板中的th:object="${user}"对应起来
+                                     @ModelAttribute(value = "user") User user,
+                                     HttpServletResponse response) {
+        //使用userService处理业务
+        Message result = userService.register(user);
+        //将结果放入model中，在模板中可以取到model中的值
+        model.addAttribute("result", result.getReason());
+        return new RedirectView("/index", true, false, true);
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String loginGet(Model model,@RequestParam(value = "result",defaultValue = "") String  result) {
+        if(result != null || result.equals("")) {
+            model.addAttribute("result", result);
+        }
+        return "login";
+    }
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public RedirectView loginPost(Model model,
+                                  @ModelAttribute(value = "user") User user,
+                                  @RequestParam(value = "sex") String sex,
+                                  HttpSession session) {
+        if (sex.equals("male")) {
+            user.setSex(1);
+        }
+        Message result = userService.login(user);
+
+        model.addAttribute("result", result.getReason());
+        if (result.isSuccess()) {
+            //添加到session中，session是一次会话，在本次会话中都可以取到session中的值
+            //若是session中有用户存在则会覆盖原来的user，当session中的user存在时判定用户存在
+            session.setAttribute("user", result.getOthers());
+            return new RedirectView("/index", true, false, true);
+        } else {
+            return new RedirectView("/login", true, false, true);
+        }
+
+    }
+
+//    @RequestMapping(value = "/loginOut", method = RequestMethod.GET)
+//    public RedirectView loginOut(HttpSession session) {
+//        //从session中删除user属性，用户退出登录
+//        session.removeAttribute("user");
+//        return new RedirectView("/index", true, false, true);
+//    }
+//
+//    @RequestMapping(value = "/userCenter", method = RequestMethod.GET)
+//    public String userCenter(HttpSession session, Model model) {
+//        User user = (User) session.getAttribute("user");
+//        if (user == null) {
+//            model.addAttribute("result", "用户未登录!");
+//            return "index";
+//        } else {
+//            model.addAttribute("user", user);
+//            return "userCenter";
+//        }
+//    }
+//
+//    @RequestMapping(value = "/user", method = RequestMethod.POST)
+//    public RedirectView user(Model model,
+//                             @ModelAttribute(value = "user") User user,
+//                             @RequestParam(value = "sex") String sex,
+//                             HttpSession session) {
+//        if (sex.equals("male")) {
+//            user.setSex(1);
+//        }
+//        Message result = userService.save(user);
+//
+//        model.addAttribute("result", result.getReason());
+//        if (result.isSuccess()) {
+//            //添加到session中，session是一次会话，在本次会话中都可以取到session中的值
+//            //若是session中有用户存在则会覆盖原来的user，当session中的user存在时判定用户存在
+//            session.setAttribute("user", result.getOthers());
+//            return new RedirectView("/index", true, false, true);
+//        } else {
+//            return new RedirectView("/login", true, false, true);
+//        }
+//    }
+//
+//    @RequestMapping(value = "/userList", method = RequestMethod.GET)
+//    public RedirectView user(Model model,
+//                             @RequestParam(value = "page", defaultValue = "1") int pageNumber,
+//                             HttpSession session) {
+//
+//        List<User> users = userService.getAllUser(pageNumber);
+//
+//        if (users != null) {
+//            //添加到session中，session是一次会话，在本次会话中都可以取到session中的值
+//            //若是session中有用户存在则会覆盖原来的user，当session中的user存在时判定用户存在
+//            model.addAttribute("userList", users);
+//            return new RedirectView("/userList", true, false, true);
+//        }
+//        model.addAttribute("result", "无结果");
+//        return new RedirectView("/userList", true, false, true);
+//    }
+
 
 }
--- src/main/java/com/tianm/domain/User.java
+++ src/main/java/com/tianm/domain/User.java
@@ -1,7 +1,6 @@
 package com.tianm.domain;
 
 
-
 import com.tianm.domain.base.BaseObject;
 
 import javax.persistence.Entity;
@@ -12,12 +11,61 @@ import javax.persistence.Entity;
 
 @Entity
 //@JsonIgnoreProperties(value={"hibernateLazyInitializer","handler","fieldHandler"})
-public class User extends BaseObject{
-
+public class User extends BaseObject {
     private String email;
-    private String name;
+    private String nickname;
     private String password;
+    private String phone;
+    private Integer sex;
+    private String profile;
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
 
 }
 

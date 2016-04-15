7d0883c63a3a1d46870c5322fe54df062b68f3c0
sunyi <sunyi@shmingjiang.org.cn>
2016-04-15 15:55:02 +0800

    2016年4月15日 星期五

--- src/main/java/com/tianm/dao/UserRepository.java
+++ src/main/java/com/tianm/dao/UserRepository.java
@@ -1,13 +1,15 @@
-package com.tianm.repository;
+package com.tianm.dao;
 
 import com.tianm.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by SunYi on 2016/3/24/0024.
  */
 @Transactional
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
 
